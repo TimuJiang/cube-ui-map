1bc49f05f2eb46a163d105103638b236f6357860
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-08-30 15:34:03 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,17 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'no-tabs': 0,
     'no-mixed-spaces-and-tabs': 0,
-    'indent': ['off', 'tab'],
+    indent: ['off', 'tab'],
     'no-trailing-spaces': 1,
-    'space-before-function-paren': 0 // 这句话表示在函数后可以不加空格
+    'space-before-function-paren': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-    devServer: {
-		proxy: {
-			'/api': {
-				target: 'http://127.0.0.1:7001',
-				changeOrigin: true
-			}
-		}
-	},
-    configureWebpack: {
-		resolve: {
-			alias: {
-				'assets': '@/assets',
-				'img': '@/assets/images',
-				'components': '@/components',
-				'pages': '@/pages',
-				'views': '@/views'
-			}
-		}
-	},
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true
+      }
+    }
+  },
 
-    runtimeCompiler: true,
-    productionSourceMap: false
+  runtimeCompiler: true,
+  productionSourceMap: false,
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  }
 }
